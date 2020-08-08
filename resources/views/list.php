aebaf220bc0c3244eed333dd967950bf91629d88
RomaVerst <r__kasatki@mail.ru>
2020-08-08 14:46:29 +0500

    lesson1

---
+++ resources/views/list.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Список новостей</title>
+</head>
+<body>
+<?include_once 'menu.php'?>
+Список новостей
+<a href="/list/item">Одна новость</a>
+</body>
+</html>
---
+++ resources/views/menu.php
@@ -0,0 +1,11 @@
+<ul>
+    <li>
+        <a href="/">Главная</a>
+    </li>
+    <li>
+        <a href="/list">Список новостей</a>
+    </li>
+    <li>
+        <a href="/info">О нас</a>
+    </li>
+</ul>
